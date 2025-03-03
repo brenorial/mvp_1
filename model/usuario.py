0bc421466afc16d76eeb9154ca9b3894da3c5fc7
brenorial <brenoalmeida.riosaude@gmail.com>
2025-03-03 12:22:32 -0300

    criado tabela de usuario

--- model/usuario.py
+++ model/usuario.py
@@ -1,18 +1,15 @@
 from sqlalchemy import Column, Integer, String
-from model import Base
+from model.base import Base
 
 class Usuario(Base):
     __tablename__ = 'usuarios'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    nome = Column(String(100), nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
-    senha = Column(String(255), nullable=False)
+    id = Column(Integer, primary_key=True)
+    nome = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    senha = Column(String, nullable=False)
 
     def __init__(self, nome, email, senha):
         self.nome = nome
         self.email = email
         self.senha = senha
-
-    def __repr__(self):
-        return f"<Usuario(id={self.id}, nome={self.nome}, email={self.email})>"
